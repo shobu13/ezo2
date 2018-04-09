38305aa6b43f26c928fd1a0cf3639e7b0cd2c873
shobu13 <shobu13@hotmail.fr>
2018-04-09 22:02:57 +0200

    ajout du modèle Profil

--- ezo2/core/models.py
+++ ezo2/core/models.py
@@ -3,4 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profil(models.Model):
-    """"""
+    """Extension du modèle utilisateur de base pour rajouter des champs de profil."""
+    user = models.OneToOneField(User)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return "Profil de {0}".format(self.user.username)
