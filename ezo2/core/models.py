0e8cee6fd69242995ba646fe640ce017f136baab
shobu13 <shobu13@hotmail.fr>
2018-04-09 21:40:23 +0200

    random

--- ezo2/core/models.py
+++ ezo2/core/models.py
@@ -1,5 +1,6 @@
+"""moduel contenant les modèles de l'application core"""
 from django.db import models
 from django.contrib.auth.models import User
 
 class Profil(models.Model):
-    pass
+    """"""
