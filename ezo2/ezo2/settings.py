30ddcfadc9a905122d36a52c67dcfedb18da7612
shobu13 <shobu13@hotmail.fr>
2018-04-11 14:27:59 +0200

    modification de l'arborescence des dossiers

--- ezo2/ezo2/settings.py
+++ ezo2/ezo2/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "core/static"),
+
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
