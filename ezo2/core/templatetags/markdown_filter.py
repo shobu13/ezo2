88845e86a72fa198a81fb04226d24954221e401d
shobu13 <shobu13@hotmail.fr>
2018-04-12 07:32:04 +0200

    bruh

--- ezo2/core/templatetags/markdown_filter.py
+++ ezo2/core/templatetags/markdown_filter.py
@@ -1,9 +1,9 @@
 from django import template
-import markdown
+from markdownx import utils
 
 register = template.Library()
 
 @register.filter
 def markdownify(text):
     # safe_mode governs how the function handles raw HTML
-    return markdown.markdown(text, safe_mode='escape')
+    return utils.markdown(text, safe_mode='escape')
