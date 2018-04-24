48fba9e2b7ee7b2386a1f2df351cb68afcecc22f
Unknown <shobu13@hotmail.fr>
2018-04-24 21:37:43 +0200

    Merge branch 'Prod' to testBranch

--- ezo2/ezo2/settings.py
+++ ezo2/ezo2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bsmb%s5y(w=e1ffcs1x%2asg2-u_u$)h4+dgqu-@=5r(v1%q!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,6 +125,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
