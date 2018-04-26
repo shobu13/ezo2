8fa891cd0805b78d46a3156ca0068f43e6c36091
Unknown <shobu13@hotmail.fr>
2018-04-26 15:54:18 +0200

    paramètrage de prod

--- ezo2/ezo2/settings.py
+++ ezo2/ezo2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bsmb%s5y(w=e1ffcs1x%2asg2-u_u$)h4+dgqu-@=5r(v1%q!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
