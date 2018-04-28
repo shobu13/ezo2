9b976bab4e835301f6812436a94443597b3774c2
Unknown <shobu13@hotmail.fr>
2018-04-28 21:09:23 +0200

    modification présentation administration paramètre

--- ezo2/core/admin.py
+++ ezo2/core/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from core.models import Profil, Parametres
 
+
+class ParametreAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Nom', {
+            'fields': ['nom']
+        }),
+        ('Valeur', {
+            'classes': ['collapse', ],
+            'description': 'le formulaire accepte les balises Markdown',
+            'fields': ['valeur']
+        }),
+    )
+
+
 admin.site.register(Profil)
-admin.site.register(Parametres)
+admin.site.register(Parametres, ParametreAdmin)
